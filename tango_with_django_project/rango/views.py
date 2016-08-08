2b263608bfa60f9c3cde3d5540766826f632cd98
Neil Bruce <Neil.Bruce@h-its.org>
2016-08-08 14:18:31 +0200

    Added home and about views with links between

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("""Rango says hey there world! 
+        <br/>See the <a href='/rango/about'>About</a> page.""")
+
+def about(request):
+    return HttpResponse("""Rango says here is the about page.
+        <br/> To go back to the index page 
+        click <a href='/rango/'>here</a>.""")
+
