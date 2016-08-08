2a6419673cf33b73c2e9f31e23e78cd24c76dfdb
Neil Bruce <Neil.Bruce@h-its.org>
2016-08-08 14:48:43 +0200

    Added view with template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,14 +1,27 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("""Rango says hey there world! 
-        <br/>See the <a href='/rango/about'>About</a> page.""")
+# Old simple index view
+#def index(request):
+#    return HttpResponse("""Rango says hey there world! 
+#        <br/>See the <a href='/rango/about'>About</a> page.""")
 
 def about(request):
     return HttpResponse("""Rango says here is the about page.
         <br/> To go back to the index page 
         click <a href='/rango/'>here</a>.""")
 
+# New index view using template
+def index(request):
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'rango/index.html', context_dict)
