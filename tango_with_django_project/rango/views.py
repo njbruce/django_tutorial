208c0e2f1ca5fb54d4bdb6ec7411ccf7f1d26c8a
njbruce <njbruce@gmail.com>
2016-08-09 21:20:36 +0200

    Index text

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -18,7 +18,7 @@ def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    context_dict = {'boldmessage': "This is the text to be added to the template."}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
