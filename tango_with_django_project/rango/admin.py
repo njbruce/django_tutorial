02cf09a2a6130ec1fabe4a0a4eb46f742e227d28
Neil Bruce <Neil.Bruce@h-its.org>
2016-08-11 11:52:19 +0200

    Updated models to include UserProfile and migrate

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # Add in this class to customized the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,4 +8,5 @@ class CategoryAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Page)
+admin.site.register(UserProfile)
 
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.conf.global_settings import PASSWORD_HASHERS
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -99,5 +100,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
 
+PASSWORD_HASHERS = (
+                    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+                    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+                    )
 
 
